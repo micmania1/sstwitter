3252991cb26d7c56b66d4283f5f6b3315fb80fec
micmania1 <micmania@hotmail.co.uk>
2013-08-12 10:58:30 +0100

    Added latest tweets widget

---
+++ code/widgets/LatestTweetsWidget.php
@@ -0,0 +1,62 @@
+<?php
+
+class LatestTweetsWidget extends Widget {
+    
+    static $title = "Latest Tweets";
+
+    static $cmsTitle = "Latest Tweets";
+
+    static $db = array(
+        "TwitterHandle" => "Varchar(255)",
+        "NumberOfTweets" => "Int",
+    );
+
+    public function Tweets() {
+        $twitterApp = TwitterApp::get()->first();
+        if(!$twitterApp) return null;
+
+        $siteConfig = SiteConfig::current_site_config();
+        $twitter = $twitterApp->getTwitter();
+        $twitter->setAccess(new OAuthToken($twitterApp->TwitterAccessToken, $twitterApp->TwitterAccessSecret));
+
+        if($twitter->hasAccess()) {
+            $result = $twitter->api("1.1/statuses/user_timeline.json", "GET", array(
+                "screen_name" => $this->TwitterHandle,
+                "count" => $this->NumberOfTweets
+            ));
+
+            if($result->statusCode() == 200) {
+               $rawTweets = json_decode($result->body(), true);
+               if(count($rawTweets) > 0) {
+                    $tweets = new ArrayList();
+                    foreach($rawTweets as $tweet) {
+                        // Parse tweet links, users and hashtags.
+                        $parsed = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a href=\"\\2\" target=\"_blank\">\\2</a>'", $tweet['text']);
+                        $parsed = preg_replace("#(^|[\n ])@([A-Za-z0-9\_]*)#ise", "'\\1<a href=\"http://www.twitter.com/\\2\" target=\"_blank\">@\\2</a>'", $parsed);
+                        $parsed = preg_replace("#(^|[\n ])\#([A-Za-z0-9]*)#ise", "'\\1<a href=\"http://www.twitter.com/search?q=\\2\" target=\"_blank\">#\\2</a>'", $parsed);
+
+                        $t = new ArrayData(array());
+                        $t->Tweet = DBField::create_field("HTMLText", $parsed, "Tweet");
+                        $t->TweetDate = DBField::create_field("SS_Datetime", strtotime($tweet['created_at']));
+                        $t->TweetLink = DBField::create_field("Varchar", "http://www.twitter.com/" . rawurlencode($tweet['user']['screen_name']) . "/status/" . rawurlencode($tweet['id_str']));
+                        $tweets->push($t);
+                    }
+                    return $tweets;
+                }
+            }
+        }
+        return null;
+    }
+
+    public function getCMSFields() {
+        $fields = new FieldList();
+        $fields->push(TextField::create("TwitterHandle", "Twitter Handle"));
+        $fields->push(NumericField::create("NumberOfTweets", "Number of Tweets"));
+        return $fields;
+    }
+
+}
+
+class LatestTweets_Controller extends Widget_Controller {
+    
+}
